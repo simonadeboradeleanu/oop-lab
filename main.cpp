7a60fa8303e4858aac3adfe17b5afec77722e5b5
simonadeboradeleanu <99869037+simonadeboradeleanu@users.noreply.github.com>
2022-02-17 10:52:06 +0200

    prob

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+int a, b, i, d, s;
 
 int main() {
-    std::cout << "Hello, world!\n";
+
+    std::cin>>a>>b;
+    for (i=a;i<=b;i++) {
+        s=0;
+        for (d = 1; d <= i / 2; d++)
+            if (i % d == 0)
+                s = s + d;
+        if(s==i)
+            std::cout<<i;
+    }
+    //std::cout << "Hello, world!\n";
     return 0;
 }
